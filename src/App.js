849afa7ac952569c272b7b932738b81ae316e28d
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 21:02:50 -0500

    feature added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import posts from './posts'
-
+import posts from './posts';
 
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+  }
+
+  updateInput(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+
   render() {
+    const inputlow = this.state.input.toLowerCase();
+    const list = posts
+      .filter((post) => post.title.toLowerCase().includes(inputlow))
+      .map((post, index) => (
+        <li key={index}>
+          <a href={post.url}>
+            <img src={post.image} alt={'image of ' + post.title} />
+          </a>
+          <p>{post.title}</p>
+        </li>
+      ));
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.updateInput.bind(this)}
+            placeholder="Ingresa el término de búsqueda"
+          />
         </div>
-        <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
-        </ul>
+        <ul>{list}</ul>
       </div>
-    )
+    );
   }
 }
 
-
-export default App
-
-
+export default App;
